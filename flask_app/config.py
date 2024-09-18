415a2cfeb9e7ad1887e0ab74123b26ecd504bde7
Ankit P <ankitpenmatcha@outlook.com>
2024-09-18 03:58:21 -0400

    set up

--- flask_app/config.py
+++
@@ -1,3 +0,0 @@
-# Stores all configuration values
-SECRET_KEY = 'os.urandom(16)'
-MONGODB_HOST = 'mongodb+srv://ankit:0tlAb30Bl4PecH7K@cluster0.5vrv3vh.mongodb.net/my_db?retryWrites=true&w=majority&appName=Cluster0'
